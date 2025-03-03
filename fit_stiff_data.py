33f34bd2d4554659de5bbdac61cc0a2017aac3f2
Mack <mackinley.shaw@anu.edu.au>
2025-03-03 14:58:52 +1100

    span selector

--- fit_stiff_data.py
+++ fit_stiff_data.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import re
 import sys
+import time
 
 from matplotlib.widgets import SpanSelector
 from matplotlib.widgets import Button
@@ -20,8 +21,10 @@ class RunStiffSelector:
         self.xlxs = pd.ExcelFile(filename)
 
         self.data = {}
-        
         num_loaded = 0
+
+        self.current_x = np.empty(0)
+        self.current_y = np.empty(0)
 
         for name in reversed(self.xlxs.sheet_names):
             if not re.match("^[A-Z]\\d+$", name): continue
@@ -39,11 +42,19 @@ class RunStiffSelector:
 
         self.fig.canvas.mpl_connect('key_press_event', self.on_press)
 
+        self.span = SpanSelector(self.ax, self.on_select, "horizontal", useblit=True, props=dict(alpha=0.5, facecolor="tab:blue"), interactive=True, drag_from_anywhere=True)
+
 
     def run(self):
         plt.show()
 
     def update_plot(self):
+        self.ax.cla()
+        if not self.data:
+            self.ax.set_title("Curve fitting complete, shutting down")
+            plt.show()
+            plt.pause(5)
+            sys.exit(0)
         sample_name, df = self.data.popitem()
         
         self.current_name = sample_name
@@ -51,12 +62,17 @@ class RunStiffSelector:
         self.current_y = df["Force"].to_numpy()
 
         self.ax.plot(self.current_x, self.current_y)
-        self.ax.relim()
+        self.ax.set_title("Sample: %s" % sample_name)
+        plt.show()
 
     def on_press(self, event):
         sys.stdout.flush()
         if event.key == "enter":
             self.update_plot()
+
+    def on_select(self, xmin, xmax):
+        indmin, indmax = np.searchsorted(self.current_x, (xmin, xmax))
+        indmax = min(len(self.current_x) - 1, indmax)
 
 if __name__ == "__main__":
     stiff = RunStiffSelector(filename, translate, row_skip)
